a0eb03884217c7f4fe45ea3b8ac8b1e480a957de
Алексей <@DESKTOP-J2QN423>
2017-09-21 22:46:12 +0300

    upd #2
    Add alert service. Finish login.component

--- ToDoApplication/src/components/login/login.component.ts
+++ ToDoApplication/src/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../../service/authentication.service';
+import { AlertService } from '../../service/alert.service';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -16,11 +18,27 @@ export class LoginComponent implements OnInit {
     loading: boolean = false;
 
     constructor(private route: ActivatedRoute,
-                private router: Router) {
+                private router: Router,
+                private authenticationService: AuthenticationService,
+                private alertService: AlertService) {
 
     }
 
     ngOnInit() {
+        this.authenticationService.logout();
+        this.returnedUrl = this.route.snapshot.queryParams['returnedUrl'] || '/';
+    }
 
+    login(){
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(data => {
+                this.router.navigate([this.returnedUrl]);
+            },
+            error =>{
+                this.alertService.error(error);
+                this.loading = false;
+            });
+            
     }
 }
